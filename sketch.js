074b687e89b1d85256abf01c2afb4025a9dabab1
dvdjcw <70393261+dvdjcw@users.noreply.github.com>
2020-09-03 14:49:16 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,16 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine,world;
+var object;
+
 function setup() {
     createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+object = Bodies.rectangle(200,100,50,50);
+World.add(world.object);    
 }
   
   function draw() {
